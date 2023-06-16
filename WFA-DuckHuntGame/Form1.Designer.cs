36c151a1d0f9dae72bab4cb2a17e53ec31e7ee7f
EnesFevzi <enssfevzi@gmail.com>
2023-06-16 12:47:01 +0300

    Add project files.

---
+++ WFA-DuckHuntGame/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WFA_DuckHuntGame
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.pnlUcusAlani = new System.Windows.Forms.Panel();
+			this.flpAtisHakki = new System.Windows.Forms.FlowLayoutPanel();
+			this.lblToplamOrdek = new System.Windows.Forms.Label();
+			this.lblVurulanOrdek = new System.Windows.Forms.Label();
+			this.lblToplamOrdekSayi = new System.Windows.Forms.Label();
+			this.lblVurulanOrdekSayi = new System.Windows.Forms.Label();
+			this.tmrYatay = new System.Windows.Forms.Timer(this.components);
+			this.tmrDikey = new System.Windows.Forms.Timer(this.components);
+			this.tmrOluOrdek = new System.Windows.Forms.Timer(this.components);
+			this.tmrKopekCikis = new System.Windows.Forms.Timer(this.components);
+			this.tmrKopekGiris = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// pnlUcusAlani
+			// 
+			this.pnlUcusAlani.BackColor = System.Drawing.Color.Transparent;
+			this.pnlUcusAlani.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pnlUcusAlani.Location = new System.Drawing.Point(0, 0);
+			this.pnlUcusAlani.Name = "pnlUcusAlani";
+			this.pnlUcusAlani.Size = new System.Drawing.Size(917, 404);
+			this.pnlUcusAlani.TabIndex = 0;
+			this.pnlUcusAlani.Click += new System.EventHandler(this.pnlUcusAlani_Click);
+			// 
+			// flpAtisHakki
+			// 
+			this.flpAtisHakki.BackColor = System.Drawing.Color.Transparent;
+			this.flpAtisHakki.Location = new System.Drawing.Point(12, 470);
+			this.flpAtisHakki.Name = "flpAtisHakki";
+			this.flpAtisHakki.Size = new System.Drawing.Size(200, 72);
+			this.flpAtisHakki.TabIndex = 1;
+			// 
+			// lblToplamOrdek
+			// 
+			this.lblToplamOrdek.AutoSize = true;
+			this.lblToplamOrdek.BackColor = System.Drawing.Color.Transparent;
+			this.lblToplamOrdek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblToplamOrdek.Location = new System.Drawing.Point(719, 470);
+			this.lblToplamOrdek.Name = "lblToplamOrdek";
+			this.lblToplamOrdek.Size = new System.Drawing.Size(149, 24);
+			this.lblToplamOrdek.TabIndex = 2;
+			this.lblToplamOrdek.Text = "Toplam Ördek:";
+			// 
+			// lblVurulanOrdek
+			// 
+			this.lblVurulanOrdek.AutoSize = true;
+			this.lblVurulanOrdek.BackColor = System.Drawing.Color.Transparent;
+			this.lblVurulanOrdek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblVurulanOrdek.Location = new System.Drawing.Point(719, 505);
+			this.lblVurulanOrdek.Name = "lblVurulanOrdek";
+			this.lblVurulanOrdek.Size = new System.Drawing.Size(152, 24);
+			this.lblVurulanOrdek.TabIndex = 2;
+			this.lblVurulanOrdek.Text = "Vurulan Ördek:";
+			// 
+			// lblToplamOrdekSayi
+			// 
+			this.lblToplamOrdekSayi.AutoSize = true;
+			this.lblToplamOrdekSayi.BackColor = System.Drawing.Color.Transparent;
+			this.lblToplamOrdekSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblToplamOrdekSayi.Location = new System.Drawing.Point(871, 470);
+			this.lblToplamOrdekSayi.Name = "lblToplamOrdekSayi";
+			this.lblToplamOrdekSayi.Size = new System.Drawing.Size(21, 24);
+			this.lblToplamOrdekSayi.TabIndex = 2;
+			this.lblToplamOrdekSayi.Text = "0";
+			// 
+			// lblVurulanOrdekSayi
+			// 
+			this.lblVurulanOrdekSayi.AutoSize = true;
+			this.lblVurulanOrdekSayi.BackColor = System.Drawing.Color.Transparent;
+			this.lblVurulanOrdekSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblVurulanOrdekSayi.Location = new System.Drawing.Point(871, 505);
+			this.lblVurulanOrdekSayi.Name = "lblVurulanOrdekSayi";
+			this.lblVurulanOrdekSayi.Size = new System.Drawing.Size(21, 24);
+			this.lblVurulanOrdekSayi.TabIndex = 2;
+			this.lblVurulanOrdekSayi.Text = "0";
+			// 
+			// tmrYatay
+			// 
+			this.tmrYatay.Interval = 10;
+			this.tmrYatay.Tick += new System.EventHandler(this.tmrYatay_Tick);
+			// 
+			// tmrDikey
+			// 
+			this.tmrDikey.Interval = 10;
+			this.tmrDikey.Tick += new System.EventHandler(this.tmrDikey_Tick);
+			// 
+			// tmrOluOrdek
+			// 
+			this.tmrOluOrdek.Interval = 10;
+			this.tmrOluOrdek.Tick += new System.EventHandler(this.tmrOluOrdek_Tick);
+			// 
+			// tmrKopekCikis
+			// 
+			this.tmrKopekCikis.Interval = 10;
+			this.tmrKopekCikis.Tick += new System.EventHandler(this.tmrKopekCikis_Tick);
+			// 
+			// tmrKopekGiris
+			// 
+			this.tmrKopekGiris.Interval = 10;
+			this.tmrKopekGiris.Tick += new System.EventHandler(this.tmrKopekGiris_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::WFA_DuckHuntGame.Properties.Resources.stage;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.ClientSize = new System.Drawing.Size(917, 554);
+			this.Controls.Add(this.lblVurulanOrdekSayi);
+			this.Controls.Add(this.lblVurulanOrdek);
+			this.Controls.Add(this.lblToplamOrdekSayi);
+			this.Controls.Add(this.lblToplamOrdek);
+			this.Controls.Add(this.flpAtisHakki);
+			this.Controls.Add(this.pnlUcusAlani);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel pnlUcusAlani;
+		private System.Windows.Forms.FlowLayoutPanel flpAtisHakki;
+		private System.Windows.Forms.Label lblToplamOrdek;
+		private System.Windows.Forms.Label lblVurulanOrdek;
+		private System.Windows.Forms.Label lblToplamOrdekSayi;
+		private System.Windows.Forms.Label lblVurulanOrdekSayi;
+		private System.Windows.Forms.Timer tmrYatay;
+		private System.Windows.Forms.Timer tmrDikey;
+		private System.Windows.Forms.Timer tmrOluOrdek;
+		private System.Windows.Forms.Timer tmrKopekCikis;
+		private System.Windows.Forms.Timer tmrKopekGiris;
+	}
+}
+
---
+++ WFA-DuckHuntGame/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_DuckHuntGame
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		Random random = new Random();
+		PictureBox pbOrdek;
+		PictureBox pbKopek = new PictureBox();
+		int atisHakki = 3;
+		int toplamOrdekSayisi;
+		int vurulanOrdekSayisi;
+
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			AtisHakkiDoldur();
+			SesCal(WFA_DuckHuntGame.Properties.Resources.intro);
+			pbKopek.Image = WFA_DuckHuntGame.Properties.Resources._2;
+			pbKopek.Width = 120;
+			pbKopek.Height = 90;
+			pbKopek.SizeMode = PictureBoxSizeMode.StretchImage;
+			pbKopek.Top = pnlUcusAlani.Height;
+			pbKopek.Left = (pnlUcusAlani.Width / 2) - (pbKopek.Width / 2);
+
+
+
+			pnlUcusAlani.Controls.Add(pbKopek);
+
+			pbOrdek = new PictureBox();
+			pbOrdek.Image = WFA_DuckHuntGame.Properties.Resources.pngegg__1_;
+			pbOrdek.Width = 120;
+			pbOrdek.Height = 80;
+			pbOrdek.Click += PbOrdek_Click;
+
+
+
+			pbOrdek.SizeMode = PictureBoxSizeMode.StretchImage;
+			pnlUcusAlani.Controls.Add(pbOrdek);
+			tmrYatay.Start();
+			tmrDikey.Start();
+		}
+
+		private void AtisHakkiDoldur()
+		{
+			flpAtisHakki.Controls.Clear();
+			for (int i = 0; i < atisHakki; i++)
+			{
+				PictureBox picturebox = new PictureBox();
+				picturebox.Width = 35;
+				picturebox.Height = 70;
+
+				picturebox.Image = WFA_DuckHuntGame.Properties.Resources.bullet_gun_bullet_pointed_images_3;
+				picturebox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+				flpAtisHakki.Controls.Add(picturebox);
+			}
+		}
+
+		private void PbOrdek_Click(object sender, EventArgs e)
+		{
+
+			tmrDikey.Stop();
+			tmrYatay.Stop();
+			AtisHakkiAzalt();
+			
+
+			if (Convert.ToBoolean(pbOrdek.Tag))
+			{
+				pbOrdek.Image = WFA_DuckHuntGame.Properties.Resources.pngegg__1___3_;
+			}
+			else
+			{
+				pbOrdek.Image = WFA_DuckHuntGame.Properties.Resources.pngegg__1___2_;
+			}
+
+			tmrOluOrdek.Start();
+			vurulanOrdekSayisi++;
+			lblVurulanOrdekSayi.Text=vurulanOrdekSayisi.ToString();
+
+
+		}
+
+		private void AtisHakkiAzalt()
+		{
+			atisHakki--;
+			flpAtisHakki.Controls.RemoveAt(flpAtisHakki.Controls.Count - 1);
+			if (atisHakki==0)
+			{
+				MessageBox.Show("Merminiz Kalmamıştır", "DUCK HUNT");
+				Application.Exit();
+			}
+		}
+
+		int yatayHiz = 5;
+
+		private void tmrYatay_Tick(object sender, EventArgs e)
+		{
+			if (pbOrdek.Right >= pnlUcusAlani.Width)
+			{
+				pbOrdek.Tag = false;
+				pbOrdek.Image = WFA_DuckHuntGame.Properties.Resources.pngegg__1___1_;
+				yatayHiz = -5;
+			}
+			else if (pbOrdek.Left <= 0)
+			{
+				pbOrdek.Tag = true;
+				pbOrdek.Image = WFA_DuckHuntGame.Properties.Resources.pngegg__1_;
+				yatayHiz = 5;
+			}
+			pbOrdek.Left += yatayHiz;
+
+		}
+		int dikeyHiz = 5;
+		private void tmrDikey_Tick(object sender, EventArgs e)
+		{
+			if (pbOrdek.Bottom >= pnlUcusAlani.Height)
+			{
+				dikeyHiz = -5;
+			}
+			else if (pbOrdek.Top <= 0)
+			{
+				dikeyHiz = 5;
+			}
+			pbOrdek.Top += dikeyHiz;
+		}
+		void SesCal(Stream ses)
+		{
+			SoundPlayer soundPlayer = new SoundPlayer();
+			soundPlayer.Stream = ses;
+			soundPlayer.Play();
+		}
+
+		private void pnlUcusAlani_Click(object sender, EventArgs e)
+		{
+			AtisHakkiAzalt();
+		}
+
+		private void tmrOluOrdek_Tick(object sender, EventArgs e)
+		{
+			pbOrdek.Top += 5;
+			if (pbOrdek.Top >= pnlUcusAlani.Height)
+			{
+				tmrOluOrdek.Stop();
+				tmrKopekCikis.Start();
+			}
+			else
+			{
+				pbOrdek.Top += 5;
+			}
+		}
+
+		private void tmrKopekCikis_Tick(object sender, EventArgs e)
+		{
+			if (pbKopek.Bottom >= pnlUcusAlani.Height)
+			{
+				pbKopek.Top -= 5;
+			}
+			else
+			{
+				tmrKopekCikis.Stop();
+				System.Threading.Thread.Sleep(2000);
+				atisHakki = 3;
+				AtisHakkiDoldur();
+				tmrKopekGiris.Start();
+				toplamOrdekSayisi++;
+				lblToplamOrdekSayi.Text = toplamOrdekSayisi.ToString();
+			}
+		}
+
+		private void tmrKopekGiris_Tick(object sender, EventArgs e)
+		{
+			if (pbKopek.Top <= pnlUcusAlani.Height)
+			{
+				pbKopek.Top += 5;
+			}
+			else
+			{
+				tmrKopekGiris.Stop();
+
+				pbOrdek.Top = random.Next(0, pnlUcusAlani.Height);
+				pbOrdek.Left= random.Next(0, pnlUcusAlani.Width);
+				pbOrdek.Image = WFA_DuckHuntGame.Properties.Resources.pngegg__1___1_;
+
+				tmrDikey.Start();
+				tmrYatay.Start();
+			}
+		}
+	}
+}
